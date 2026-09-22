f8b0210f7a1591bec8d749e6d797db2791a56170
justacoder52 <Jsagar0316@gmail.com>
2026-09-22 11:51:53 -0400

    Type conversion from int to char

---
+++ Typeconversion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << 'A' + 'A' << endl; // 130
+	cout << 'A' + 1 << endl;
+
+	char ch = 'A' + 1;
+	cout << ch << endl;
+
+	cout << 'a' + 'a' << endl;
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
